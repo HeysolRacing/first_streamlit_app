25134ff760b99e026973735d0b650c62a54595f8
Heysel <heysel.sagaon@gmail.com>
2023-03-03 16:25:57 -0600

    Update streamlit_app.py
    Adding Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boilded Free Range Egg')
