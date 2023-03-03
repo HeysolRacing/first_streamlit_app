d2ffc07694a538140d6411743821f2047b914a1f
Heysel <heysel.sagaon@gmail.com>
2023-03-03 16:26:54 -0600

    Update streamlit_app.py
    Adding title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boilded Free Range Egg')
